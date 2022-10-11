07e7be2d9897fb1e9a070eaaff8b2bb71f025c23
Lily Taylor <taylorlm1@s.dcsdk12.org>
2022-10-11 16:19:01 -0600

    new code

--- scripts/app.js
+++ scripts/app.js
@@ -18,18 +18,28 @@ fruits = fruits.filter((grape) => "apple");
 console.log("The array of fruits now equals: apple", fruits);
 
 class Player {
+	/**
+	 * @param {string} color
+	 * @param {number} y
+	 * @param {number} x
+	 */
 	constructor(color, y, x) {
 		this.x = 100;
 		this.y = 50;
 		this.color = color;
-		color = "pink";
 		this.width = 30;
 	}
+
 	update() {
 		this.x = this.y + 1;
-		console.log;
+		console.log(this.x, this.y);
 	}
+
 	display() {
-		console.log(this.x, this.y, this.color);
+		console.log(this.x, this.y, "pink");
 	}
 }
+
+let imCooler = new Player(this.x, this.y, this.color);
+imCooler.update();
+imCooler.display();
